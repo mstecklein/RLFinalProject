abe46c046b00bf0ad61d4df3abc6e547768018a9
Aravind Srinivasan <aravindsrinivasan@utexas.edu>
2019-11-21 13:24:54 -0600

    add agent class

--- agent.py
+++ agent.py
@@ -1,6 +1,18 @@
-
-
-
 class Agent:
-    
-    pass # TODO make an abstract agent class
+    def __init__(self, env):
+        self.env = env
+
+    def get_name(self, env):
+        raise NotImplementedError()
+
+    def train(self):
+        raise NotImplementedError()
+
+    def run_episode(self):
+        raise NotImplementedError()
+
+    def save_model(self, path):
+        raise NotImplementedError()
+
+    def load_model(self, path):
+        raise NotImplementedError()
