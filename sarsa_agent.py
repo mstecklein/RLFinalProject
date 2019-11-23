d599de873ce21dd7c71357fea7701865d0cb72b1
Aravind Srinivasan <aravindsrinivasan@utexas.edu>
2019-11-23 16:43:31 -0600

    add training for sarsa lambda

--- sarsa_agent.py
+++ sarsa_agent.py
@@ -22,6 +22,7 @@ class SarsaAgent(Agent):
         self.env = env
         self.num_actions = env.action_space.n
         self.num_sensors = env._num_sensors
+        self.w = np.zeros(self.num_sensors)
         self.gamma = gamma
         self.lam = lam
         self.alpha = alpha
@@ -33,42 +34,46 @@ class SarsaAgent(Agent):
         w = np.zeros(self.num_sensors)
 
         for episode in range(num_episodes):
+            if episode % 100 == 0:
+                print('Episode #{}'.format(episode))
+
             state = env.reset()
             z = np.zeros(self.num_sensors)
-            sensor_state = state[SENSOR_STATUSES]
-            action = self.get_action(w, sensor_state, self.epsilon)
+            action = self.get_action(w, state, self.epsilon)
 
             while True:
-                next_state, reward, done, _  = env.step(action)
-                env.render()
+                next_state, reward, done, _ = env.step(action)
+                # env.render()
                 sensor_state = state[SENSOR_STATUSES]
                 delta = reward - np.dot(w, sensor_state)
-                print('Delta is {}'.format(delta))
                 z += sensor_state
 
                 if done:
                     w += self.alpha * delta * z
                     break
 
+                next_action = self.get_action(w, next_state, self.epsilon)
                 next_sensor_state = next_state[SENSOR_STATUSES]
-                next_action = self.get_action(w, next_sensor_state, self.epsilon)
                 delta += self.gamma * np.dot(w, next_sensor_state)
                 w += self.alpha * delta * z
                 z = self.gamma * self.lam * z
                 state = next_state
                 action = next_action
 
+        self.w = w
+
     # E-greedy
     def get_action(
             self,
             w: np.array,     # Weight vector
-            s: np.array,     # Sensor statuses
+            state: dict,     # Sensor statuses
             epsilon: float,  # eplison greedy value
     ) -> int:
 
         if np.random.rand() < self.epsilon:
             return np.random.randint(0, self.num_actions)
 
+        s = state[SENSOR_STATUSES]
         all_actions = self.get_all_valid_actions(s)
         all_future_states = []
         for action in all_actions:
@@ -105,7 +110,24 @@ class SarsaAgent(Agent):
 
         return np.array(actions)
 
+    def run_episode(self):
+        rewards = []
+        state = self.env.reset()
+        done = False
+        while not done:
+            a = self.get_action(self.w, state, 0.)
+            next_state, reward, done, _ = self.env.step(a)
+            rewards.append(reward)
+            state = next_state
+        return rewards
+
+
+
 if __name__ == '__main__':
     env = Environment_v0()
     agent = SarsaAgent(env)
-    agent.train(100)
+    agent.train(1000)
+
+    for i in range(10):
+        episode_rewards = agent.run_episode()
+        print('The episode took {} steps and had a reward of {}'.format(len(episode_rewards), sum(episode_rewards)))
