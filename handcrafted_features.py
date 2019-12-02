42eb6ced08bc0e2c00d069b47fc6f7188c84ad8e
Michael Stecklein <michaelrstecklein@gmail.com>
2019-12-02 15:42:05 -0600

    ckpt

---
+++ handcrafted_features.py
@@ -0,0 +1,275 @@
+from environment import Environment_v0
+from dqn_agent import DQNAgent
+from environment import Environment_v1, SENSOR_STATUSES, LOCATED_SOURCES, SENSOR_COVERAGES, SENSOR_OBSERVATIONS
+from always_on_agent import AlwaysOnAgent
+import torch
+from torch import nn
+import torch.optim as optim
+from collections import deque
+import numpy as np
+
+
+
+
+
+
+
+class FeatureVector:
+    
+    def get_feature_vector_len(self):
+        raise NotImplementedError
+    
+    def parse_new_state_info(self, raw_state, info):
+        # Parses environment info into raw state. Returns parsed state.
+        # NOTE: if 'info' is None, it resets all saved history
+        raise NotImplementedError
+    
+    def __call__(self, parsed_state, action):
+        # Returns feature vector X(s,a)
+        # 'state' must be a previously parsed state from parse_new_state_info().
+        raise NotImplementedError
+    
+    def get_all_feature_vectors(self, parsed_state):
+        # Returns array of all feature vectors X(s,a) for all 'a'
+        # 'state' must be a previously parsed state from parse_new_state_info().
+        raise NotImplementedError
+
+
+
+
+
+
+
+class BasicFeatureVector(FeatureVector):
+    
+    def __init__(self, env:Environment_v0):
+        self.env = env
+    
+    def get_feature_vector_len(self):
+        return self.env.action_space.n + self.env._num_sensors
+    
+    def parse_new_state_info(self, raw_state, info):
+        # Parses environment info into raw state. Returns parsed state.
+        # NOTE: if 'info' is None, it resets all saved history
+        return raw_state
+    
+    def __call__(self, parsed_state, action):
+        # Returns feature vector X(s,a)
+        # 'state' must be a previously parsed state from parse_new_state_info().
+        sensor_state = state[SENSOR_STATUSES]
+        located_state = state[LOCATED_SOURCES].flatten()
+        return np.concatenate((sensor_state, located_state))
+    
+    def get_all_feature_vectors(self, parsed_state):
+        # Returns array of all feature vectors X(s,a) for all 'a'
+        # 'state' must be a previously parsed state from parse_new_state_info().
+        x = [self(parsed_state, None)]
+        return x * self.get_feature_vector_len()
+
+
+
+
+
+
+
+
+
+class HandCraftedFeatureVector(FeatureVector):
+    """
+    Vector index descriptions:
+        0. sum of: whether each square will be 1-covered (0/1)
+        1. sum of: whether each square will be 2-covered (0/1)
+        2. sum of: whether each square will be 3-covered (0/1)
+        3. # of sensors on
+        4. # of sources found x # of sensors on
+        5. sum of: whether each square will be 2-covered (0/1) 
+             x time-discounted observation history of that square
+        6. sum of: whether each square will be 3-covered (0/1) 
+             x "                                                "
+        7. sum of: whether each square will be 2-covered (0/1)
+             x whether a source has been found or a sensor exists in each square (0/1)
+        8. sum of: whether each square will be 3-covered (0/1) 
+             x "                                                                     "
+    """
+    
+    def __init__(self, env:Environment_v0):
+        self.size = 9
+        self.env = env
+        self._X_1_on = np.zeros(self._get_feat_mat_shape())
+        self._X_no_change = np.zeros(self._get_feat_mat_shape())
+        self._X_1_off = np.zeros(self._get_feat_mat_shape())
+        self._obs_discount_factor = 0.5
+        self._scaling_factors = np.array([
+            100., # 0
+            100., # 1
+            100., # 2
+            20., # 3
+            200., # 4
+            100., # 5
+            100., # 6
+            100., # 7
+            100., # 8
+        ])
+        
+    
+    def _get_feat_mat_shape(self):
+        return self.env.field_shape + (self.get_feature_vector_len(),)
+    
+    
+    def get_feature_vector_len(self):
+        return self.size
+        
+        
+    def parse_new_state_info(self, raw_state, info):
+        # Parses environment info into raw state. Returns parsed state.
+        # NOTE: if 'info' is None, it resets all saved history
+        if info is None:
+            self._reset_things()
+        # create coverage count map
+        cov_cnt_map = np.zeros(self.env.field_shape)
+        for sensor_num in range(self.env._num_sensors):
+            if raw_state[SENSOR_STATUSES][sensor_num] == 1: # is on
+                cov_cnt_map += raw_state[SENSOR_COVERAGES][sensor_num]
+        raw_state["coveragecountmap"] = cov_cnt_map
+        # source and sensor locations
+        if self._sourcesensorlocs is None:
+            self._sourcesensorlocs = np.zeros(self.env.field_shape, dtype=np.bool)
+            for snsr_loc in self.env.sensor_locs:
+                self._sourcesensorlocs[snsr_loc] = 1
+        self._sourcesensorlocs |= raw_state[LOCATED_SOURCES].astype(np.bool)
+        raw_state["sourcesensorlocs"] = self._sourcesensorlocs
+        # number of sensors on
+        raw_state["numsensorson"] = np.sum(raw_state[SENSOR_STATUSES])
+        # number of sources found
+        raw_state["numsourcesfound"] = np.sum(raw_state[LOCATED_SOURCES])
+        # time-discounted observation history
+        self._timediscountedobshistory *= self._obs_discount_factor
+        if not info is None:
+            for obs in info[SENSOR_OBSERVATIONS]:
+                obs_map = np.ones(self.env.field_shape, dtype=np.int)
+                for sensor_num in obs:
+                    obs_map = obs_map & raw_state[SENSOR_COVERAGES][sensor_num].astype(np.int)
+                self._timediscountedobshistory += (obs_map.astype(np.float) / np.sum(obs_map))
+        raw_state["timediscountedobshistory"] = self._timediscountedobshistory
+        # return:
+        return raw_state
+        
+    
+    def _reset_things(self):
+        self._sourcesensorlocs = None
+        self._timediscountedobshistory = np.zeros(self.env.field_shape)
+    
+    
+    def __call__(self, state, action):
+        # Returns feature vector X(s,a)
+        # 'state' must be a previously parsed state from parse_new_state_info().
+        X_1_on, X_no_change, X_1_off = self._create_feature_mats(state)
+        if not action is self.env.NO_OP_ACTION:
+            snsr_num, turn_on = self.env.get_action_info(action)
+            coverage_map = state[SENSOR_COVERAGES][snsr_num]
+            coverage_map = coverage_map.reshape(coverage_map.shape + (1,))
+            X = X_no_change * (~ coverage_map.astype(np.bool))
+            if turn_on:
+                X += X_1_on * coverage_map
+            else:
+                X += X_1_off * coverage_map
+        else:
+            X = X_no_change
+        return self._create_feat_vec_from_mat(X, state, action)
+        
+        
+    def get_all_feature_vectors(self, state):
+        # Returns array of all feature vectors X(s,a) for all 'a'
+        # 'state' must be a previously parsed state from parse_new_state_info().
+        X_1_on, X_no_change, X_1_off = self._create_feature_mats(state)
+        feat_vecs = []
+        for action in range(self.env.action_space.n):
+            if not action is self.env.NO_OP_ACTION:
+                snsr_num, turn_on = self.env.get_action_info(action)
+                coverage_map = state[SENSOR_COVERAGES][snsr_num]
+                coverage_map = coverage_map.reshape(coverage_map.shape + (1,))
+                X = X_no_change * (~ coverage_map.astype(np.bool))
+                if turn_on:
+                    X += X_1_on * coverage_map
+                else:
+                    X += X_1_off * coverage_map
+            else:
+                X = X_no_change
+            x_feat_vec = self._create_feat_vec_from_mat(X, state, action)
+            feat_vecs.append(x_feat_vec)
+        return feat_vecs
+    
+    
+    def _create_feature_mats(self, s_summarized):
+        idx = 0
+        cov_cnt_map = s_summarized["coveragecountmap"]
+        #  0. whether each square will be 1-covered (0/1)
+        self._X_1_on[:,:,idx] = 1
+        self._X_no_change[:,:,idx] = cov_cnt_map >= 1
+        self._X_1_off[:,:,idx] = (cov_cnt_map - 1) >= 1
+        idx += 1
+        #  1. whether each square will be 2-covered (0/1)
+        self._X_1_on[:,:,idx] = (cov_cnt_map + 1) >= 2
+        self._X_no_change[:,:,idx] = cov_cnt_map >= 2
+        self._X_1_off[:,:,idx] = (cov_cnt_map - 1) >= 2
+        idx += 1
+        #  2. whether each square will be 3-covered (0/1)
+        self._X_1_on[:,:,idx] = (cov_cnt_map + 1) >= 3
+        self._X_no_change[:,:,idx] = cov_cnt_map >= 3
+        self._X_1_off[:,:,idx] = (cov_cnt_map - 1) >= 3
+        idx += 1
+        #  3. # of sensors on
+        num_gridsquares = cov_cnt_map.size
+        self._X_1_on[:,:,idx] = min(s_summarized["numsensorson"] + 1, self.env._num_sensors) / num_gridsquares
+        self._X_no_change[:,:,idx] = s_summarized["numsensorson"] / num_gridsquares
+        self._X_1_off[:,:,idx] = max(s_summarized["numsensorson"] - 1, 0) / num_gridsquares
+        idx += 1
+        #  4. # of sources found
+        self._X_1_on[:,:,idx] = s_summarized["numsourcesfound"] * self._X_1_on[:,:,idx-1]
+        self._X_no_change[:,:,idx] = s_summarized["numsourcesfound"] * self._X_no_change[:,:,idx-1]
+        self._X_1_off[:,:,idx] = s_summarized["numsourcesfound"] * self._X_1_off[:,:,idx-1]
+        idx += 1
+        #  5. whether each square will be 2-covered (0/1) 
+        #       x time-discounted observation history of that square
+        self._X_1_on[:,:,idx] = ((cov_cnt_map + 1) >= 2) * s_summarized["timediscountedobshistory"]
+        self._X_no_change[:,:,idx] = (cov_cnt_map >= 2) * s_summarized["timediscountedobshistory"]
+        self._X_1_off[:,:,idx] = ((cov_cnt_map - 1) >= 2) * s_summarized["timediscountedobshistory"]
+        idx += 1
+        #  6. sum of: whether each square will be 3-covered (0/1) 
+        #       x "                                                "
+        self._X_1_on[:,:,idx] = ((cov_cnt_map + 1) >= 3) * s_summarized["timediscountedobshistory"]
+        self._X_no_change[:,:,idx] = (cov_cnt_map >= 3) * s_summarized["timediscountedobshistory"]
+        self._X_1_off[:,:,idx] = ((cov_cnt_map - 1) >= 3) * s_summarized["timediscountedobshistory"]
+        idx += 1
+        #  7. whether each square will be 2-covered (0/1)
+        #       x whether a source has been found or a sensor exists in each square (0/1)
+        self._X_1_on[:,:,idx] = ((cov_cnt_map + 1) >= 2) * s_summarized["sourcesensorlocs"]
+        self._X_no_change[:,:,idx] = (cov_cnt_map >= 2) * s_summarized["sourcesensorlocs"]
+        self._X_1_off[:,:,idx] = ((cov_cnt_map - 1) >= 2) * s_summarized["sourcesensorlocs"]
+        idx += 1
+        # 8. whether each square will be 3-covered (0/1) 
+        #       x whether a source has been found or a sensor exists in each square (0/1)
+        self._X_1_on[:,:,idx] = ((cov_cnt_map + 1) >= 3) * s_summarized["sourcesensorlocs"]
+        self._X_no_change[:,:,idx] = (cov_cnt_map >= 3) * s_summarized["sourcesensorlocs"]
+        self._X_1_off[:,:,idx] = ((cov_cnt_map - 1) >= 3) * s_summarized["sourcesensorlocs"]
+        # return:
+        return self._X_1_on, self._X_no_change, self._X_1_off
+    
+    
+    def _create_feat_vec_from_mat(self, X, state_summarized, action):
+        x = np.sum(X, axis=(0,1))
+        snsr_num, turn_on = self.env.get_action_info(action)
+        # num sensors on
+        num_snsr_on = state_summarized["numsensorson"]
+        x[5] = num_snsr_on
+        if not action is self.env.NO_OP_ACTION:
+            if turn_on:
+                x[5] += 1
+            else:
+                x[5] -= 1
+        # num of sources found  x  num of sensors on
+        num_srcs_on = state_summarized["numsourcesfound"]
+        x[6] = num_srcs_on * x[5]
+        return x / self._scaling_factors
+
+
---
+++ sarsa_agent_v2.py
@@ -0,0 +1,201 @@
+import numpy as np
+import gym
+from environment import Environment_v1, SENSOR_STATUSES, LOCATED_SOURCES
+from agent import Agent
+from handcrafted_features import HandCraftedFeatureVector, FeatureVector
+import torch
+from gym.wrappers.monitoring.video_recorder import VideoRecorder
+
+'''
+SarsaAgentV1 differs from SarsaAgentV0 by taking the located sources into consideration
+'''
+
+
+class SarsaAgentV2(Agent):
+
+    def __init__(
+            self,
+            env: gym.Env,
+            X: FeatureVector,
+            gamma: float = 0.9,    # Discount factor
+            lam: float = 0.01,     # Decay Rate
+            alpha: float = 0.01,  # Step size
+            epsilon: float = 1.,
+            epsilon_decay: float = 0.999,
+            min_epsilon: float = .01,
+            debug: bool = False
+    ):
+        super().__init__(env)
+
+        self.env = env
+        self.num_actions = env.action_space.n
+        self.num_sensors = env._num_sensors
+        self.field_size = env.field_size
+        self.X = X
+        self.w = np.zeros(self.X.get_feature_vector_len())
+        self.gamma = gamma
+        self.lam = lam
+        self.alpha = alpha
+        self.epsilon = epsilon
+        self.epsilon_decay = epsilon_decay
+        self.min_epsilon = min_epsilon
+        self.episode_num = 0
+        self._debug = debug
+        self._previously_saved_w = None
+
+    def train(self, num_episodes: int):
+        
+        self.load()
+
+        start_episode = self.episode_num
+        while self.episode_num - start_episode < num_episodes:
+            if self.episode_num % 100 == 0 and self._debug:
+                print('Episode #{}'.format(self.episode_num))
+
+            raw_state = env.reset()
+            state = self.X.parse_new_state_info(raw_state, None)
+            z = np.zeros(self.X.get_feature_vector_len())
+            action = self.get_action(self.w, state, self.epsilon)
+            x = self.X(state, action)
+            Q_old = 0.
+
+            done = False
+            while not done:
+                raw_next_state, reward, done, info = env.step(action)
+                next_state = self.X.parse_new_state_info(raw_next_state, info)
+                # env.render()
+                next_action = self.get_action(self.w, next_state, self.epsilon)
+                next_x = self.X(next_state, next_action)
+                Q = np.dot(self.w, x)
+                next_Q = np.dot(self.w, next_x)
+                delta = reward + self.gamma * next_Q - Q
+                z = self.gamma*self.lam*z + (1 - self.alpha*self.gamma*self.lam*np.dot(z,x)) * x
+                self.w += self.alpha*(delta + Q - Q_old)*z - self.alpha*(Q - Q_old)*x
+                Q_old = next_Q
+                x = next_x
+                action = next_action
+            
+            if self.epsilon > self.min_epsilon:
+                self.epsilon *= self.epsilon_decay
+            self.episode_num += 1
+            if self.episode_num % 100 == 0:
+                self.save()
+    
+
+    # E-greedy
+    def get_action(
+            self,
+            w: np.array,     # Weight vector
+            state: dict,     # Sensor statuses
+            epsilon: float,  # eplison greedy value
+    ) -> int:
+
+        if np.random.rand() < self.epsilon:
+            return np.random.randint(0, self.num_actions)
+
+        valid_actions = self._get_all_valid_actions(state[SENSOR_STATUSES])
+        all_feat_vecs = self.X.get_all_feature_vectors(state)
+
+        Q = [np.dot(w, all_feat_vecs[action]) for action in valid_actions]
+
+        # The idx corresponds to the idx for the action in all_actions
+        action_idx = np.argmax(Q)
+        return valid_actions[action_idx]
+
+    def _get_all_valid_actions(
+            self,
+            s: np.array,
+    ) -> np.array:
+        actions = []
+
+        # Iterate through sensors and add valid actions
+        for i in range(len(s)):
+            if s[i] == 0:
+                actions.append(i)       # Switch on if off
+            else:
+                actions.append(i + 20)  # Switch off if on
+
+        # Append no-op action
+        actions.append(40)
+
+        return np.array(actions)
+
+    def run_episode(self, render=False):
+        self.load()
+        rewards = []
+        raw_state = self.env.reset()
+        state = self.X.parse_new_state_info(raw_state, None)
+        done = False
+        if self._debug and render:
+            video_recorder = VideoRecorder(self.env, path="/tmp/gym_%s_run.mp4"%self.get_model_filename())
+            video_recorder.capture_frame()
+        while not done:
+            # Use a greedy policy with the values learnt from training
+            a = self.get_action(self.w, state, 0.)
+            raw_next_state, reward, done, info = self.env.step(a)
+            if self._debug and render:
+                env.render()
+            next_state = self.X.parse_new_state_info(raw_next_state, info)
+            rewards.append(reward)
+            state = next_state
+            if self._debug and render:
+                video_recorder.capture_frame()
+        if self._debug and render:
+            video_recorder.close()
+        return rewards
+    
+    
+    def get_model_filename(self):
+        return "SarsaAgentV2"
+    
+    
+    def save(self):
+        # Saves Q function state
+        torch.save({
+            'episode_num' : self.episode_num,
+            'epsilon' : self.epsilon,
+            'w' : self.w
+        }, "./data/models/" + self.get_model_filename() + ".model")
+        if self._debug:
+            if not self._previously_saved_w is None:
+                print("\t\t\t\t\t\t max diff in w since last save:", np.max(np.abs(self.w-self._previously_saved_w)))
+            print("Saved w:", self.w)
+            print("Saved epsilon:", self.epsilon)
+            self._previously_saved_w = self.w.copy()
+    
+    
+    def load(self):
+        # Loads Q function state
+        try:
+            checkpoint = torch.load("./data/models/" + self.get_model_filename() + ".model")
+            self.episode_num = checkpoint['episode_num']
+            self.epsilon = checkpoint['epsilon']
+            self.w = checkpoint['w']
+            if self._debug:
+                print("Loaded w:", self.w)
+                print("Loaded epsilon:", self.epsilon)
+        except FileNotFoundError:
+            if self._debug:
+                print("Did not load a model.")
+
+
+
+
+
+
+
+
+if __name__ == '__main__':
+    env = Environment_v1(debug=False)
+    agent = SarsaAgentV2(env, HandCraftedFeatureVector(env), debug=True)#, epsilon=.5, epsilon_decay=1.)
+    #agent.train(5000)
+ 
+    env._debug = False
+    returns = []
+    num_eps = 100
+    for i in range(num_eps):
+        ep_rewards = agent.run_episode(True)
+        print("TOTAL:", sum(ep_rewards))
+        returns.append(sum(ep_rewards))
+    print("SarsaAgentV2, %d episodes:" % num_eps)
+    print("Return:    mean:%.2f    std:%.2f" % (np.mean(returns), np.std(returns)))
